d5748027c3982d69cb212d2d31d301c7c5d6db34
Rushi,Ganmukhi <rganmukh@litle.com>
2012-02-09 09:37:23 -0500

    changed css file, app works in chrome, IE, firefox

--- test/unit/capture_test.rb
+++ test/unit/capture_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class CaptureTest < ActiveSupport::TestCase
    test "the truth" do
      @capture = captures(:one)
+     get :show
+     puts @post 
+     puts @message
      assert_equal(nil,@post)
      assert_equal("",@message)
    end
