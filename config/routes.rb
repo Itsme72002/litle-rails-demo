e82064641abad2b5d5c65c4fe61bcdabc8b83c0a
arch85 <archit.agarwal@gmail.com>
2012-02-15 16:23:59 -0500

    renaming book to welcome

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,6 @@ Demo::Application.routes.draw do
 
   resources :orders
 
-  get "book/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -64,8 +62,7 @@ Demo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-    root :to => 'book#index'
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
